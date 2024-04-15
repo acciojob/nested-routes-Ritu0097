a76c34dde2fbb9055f46205cc0294feec9e769e1
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-15 19:37:35 +0530

    women

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import './../styles/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './Layout';
+
 const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
       <Router>
         <Switch>
-          <Route path="/" component={Layout} />
+          <Route path="/" render={() => <Layout />} />
         </Switch>
       </Router>
     </div>
   )
 }
 
-export default App
+export default App;
