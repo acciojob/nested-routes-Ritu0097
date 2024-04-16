c9cb1f7a22ce97025cb6a8da55ae89a6ce10b6c1
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-16 18:54:54 +0530

    women

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Topics from "./Topics.js";
 function App() {
   return (
       <div>
-        <BrowserRouter>
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/Items">Women</Link>
+            <Link to="/Women">Women</Link>
           </li>
         </ul>
         <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/Items/*"element={<Topics/>} />
+        <Route path="/Women/*"element={<Topics/>} />
         </Routes>
-        </BrowserRouter>
       </div>
   );
 }
